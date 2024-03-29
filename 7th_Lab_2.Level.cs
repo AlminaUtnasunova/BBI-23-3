d695e88efab0c14bf612851743022d27c37fb061
AlminaUtnasunova <159530330+AlminaUtnasunova@users.noreply.github.com>
2024-03-29 21:25:50 +0300

    Add files via upload

---
+++ 7th_Lab_2.Level.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Human
+{
+    private static int nextId = 1001;
+    private int _studentId;
+    protected string _name;
+    protected int _mathGrade;
+    protected int _physicsGrade;
+    protected int _russianGrade;
+
+    public Human(string name, int mathGrade, int physicsGrade, int russianGrade)
+    {
+        _name = name;
+        _mathGrade = mathGrade;
+        _physicsGrade = physicsGrade;
+        _russianGrade = russianGrade;
+        _studentId = nextId;
+        nextId++;
+    }
+
+    protected virtual double AverageGrade()
+    {
+        return (_mathGrade + _physicsGrade + _russianGrade) / 3.0;
+    }
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine("Name: {0}, Student ID: {1}, Average Grade: {2:f2}", _name, StudentId, AverageGrade());
+    }
+
+    public int StudentId { get { return _studentId; } }
+}
+
+class Student : Human
+{
+    public Student(string name, int mathGrade, int physicsGrade, int russianGrade)
+        : base(name, mathGrade, physicsGrade, russianGrade)
+    {
+    }
+
+    protected override double AverageGrade()
+    {
+        return (_mathGrade + _physicsGrade + _russianGrade) / 3.0;
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Name: {0}, ID: {1}, Average Grade: {2:f2}", _name, StudentId, AverageGrade());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student[] students = new Student[8];
+        students[0] = new Student("Alice", 5, 4, 5);
+        students[1] = new Student("Bob", 4, 3, 4);
+        students[2] = new Student("Karl", 3, 5, 4);
+        students[3] = new Student("David", 5, 3, 4);
+        students[4] = new Student("Eve", 4, 5, 3);
+        students[5] = new Student("Emma", 5, 5, 5);
+        students[6] = new Student("Alex", 2, 3, 5);
+        students[7] = new Student("Lili", 3, 2, 4);
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            students[i].PrintInfo();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ 7th_Lab_3.Level.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+class Team
+{
+    private string Name { get; }
+    private int Points { get; }
+
+    public Team(string name, int points)
+    {
+        Name = name;
+        Points = points;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public int GetPoints()
+    {
+        return Points;
+    }
+
+    public virtual void Print(int place)
+    {
+        Console.WriteLine("{0}. {1} команда {2} баллов", place, GetName(), GetPoints());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Team[] womenTeams = new Team[7]
+        {
+            new Team("женская Team1", 26),
+            new Team("женская Team2", 21),
+            new Team("женская Team3", 19),
+            new Team("женская Team4", 16),
+            new Team("женская Team5", 25),
+            new Team("женская Team6", 15),
+            new Team("женская Team7", 17)
+        };
+        SortTeams(womenTeams);
+
+        Team[] menTeams = new Team[7]
+        {
+            new Team("мужская Team8", 18),
+            new Team("мужская Team9", 23),
+            new Team("мужская Team10", 20),
+            new Team("мужская Team11", 16),
+            new Team("мужская Team12", 24),
+            new Team("мужская Team13", 22),
+            new Team("мужская Team14", 28)
+        };
+        SortTeams(menTeams);
+
+        Team[] topTeams = new Team[12];
+
+        int womenIndex = 0;
+        int menIndex = 0;
+
+        for (int i = 0; i < topTeams.Length; i++)
+        {
+            if (i < 6)
+            {
+                topTeams[i] = womenTeams[womenIndex];
+                womenIndex++;
+            }
+            else
+            {
+                topTeams[i] = menTeams[menIndex];
+                menIndex++;
+            }
+        }
+
+        SortTeams(topTeams);
+
+        Console.WriteLine("Топ 12 команд:");
+        for (int i = 0; i < 12; i++)
+        {
+            topTeams[i].Print(i + 1);
+        }
+
+        Console.ReadKey();
+    }
+
+    static void SortTeams(Team[] teams)
+    {
+        for (int i = 0; i < teams.Length; i++)
+        {
+            for (int j = i + 1; j < teams.Length; j++)
+            {
+                if (teams[j].GetPoints() > teams[i].GetPoints())
+                {
+                    Team temp = teams[j];
+                    teams[j] = teams[i];
+                    teams[i] = temp;
+                }
+            }
+        }
+    }
+}
